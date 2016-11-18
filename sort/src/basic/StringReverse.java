a6350f0ad1987e799e2f176e06ef5899b2280c78
cigg93 <cigg153@163.com>
2016-11-18 17:55:46 +0800

    字符串反转

---
+++ sort/src/basic/StringReverse.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String originStr = "123456";
+		System.out.println(reverse(originStr));
+
+	}
+	
+	
+	public static String reverse(String originStr) {
+        if(originStr == null || originStr.length() <= 1) 
+            return originStr;
+        return reverse(originStr.substring(1)) + originStr.charAt(0);
+    }
+
+}
