f1c52b5f2e9e7b5f33d13c7d5c318f7e61625ea5
cigg93 <cigg153@163.com>
2016-11-22 11:52:47 +0800

    Cons.java

--- cartuning/src/com/cc/base/cons/Cons.java
+++ cartuning/src/com/cc/base/cons/Cons.java
@@ -1,11 +1,11 @@
 package com.cc.base.cons;
 
+/**
+ * 公共常量
+ * @author Administrator
+ *
+ */
 public class Cons {
 	
-	public static final String ITEMCUSTOM_GETITEMCUSTOMS = "ItemCustom.getItemCustoms";
-	
-	public static final String ITEMCUSTOM_INSERTITEMCUSTOMS = "ItemCustom.insertItemCustoms";
-	
-	public static final String USER_LOGIN = "User.login";
 	
 }
