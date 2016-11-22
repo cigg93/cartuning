6c27b83430e21f0f6d138b2e51301d6e4cb60ead
cigg93 <cigg153@163.com>
2016-11-22 15:00:46 +0800

    BookCatagory.java

---
+++ cartuning/src/com/cc/library/entity/BookCatagory.java
@@ -0,0 +1,22 @@
+package com.cc.library.entity;
+
+public class BookCatagory {
+	private Integer id;
+	private String catagoryName;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCatagoryName() {
+		return catagoryName;
+	}
+	public void setCatagoryName(String catagoryName) {
+		this.catagoryName = catagoryName;
+	}
+	
+	
+
+}
