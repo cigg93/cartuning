7af002e0d19ca7d3d3ec5729add82545dbb083aa
cigg93 <cigg153@163.com>
2016-11-22 11:54:41 +0800

    Result.java

--- cartuning/src/com/cc/base/entity/Result.java
+++ cartuning/src/com/cc/base/entity/Result.java
@@ -6,9 +6,28 @@ public class Result {
 	
 	public static final String FAILIURE = "failure";
 	
+	/**
+	 * 状态码
+	 */
+	private int code;
+	
+	/**
+	 * 返回结果
+	 */
 	private String result;
 	
+	/**
+	 * 返回消息
+	 */
 	private String msg;
+	
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
 
 	public String getResult() {
 		return result;
